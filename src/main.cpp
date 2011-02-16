7436cd321c19593548bfecbc04a4cad7e536ddb5
christophe.duvernois <christophe.duvernois@17a369b2-0695-9883-d3f2-36c5a1934f31>
2011-02-16 19:58:03 +0000

    fix bug qscintilla macro : null char at the end .

--- src/main.cpp
+++ src/main.cpp
@@ -4,28 +4,28 @@
 #include "mainwindow.h"
 
 int main(int argc, char *argv[]) {
-    Q_INIT_RESOURCE(ressources);
+	Q_INIT_RESOURCE(ressources);
 
 	QString message;
-    for (int a = 1; a < argc; ++a) {
+	for (int a = 1; a < argc; ++a) {
 		message += argv[a];
 		if (a < argc-1)
 			message += " ";
-    }
-	
-    //QApplication app(argc, argv);
+	}
+
+	//QApplication app(argc, argv);
 	QtSingleApplication app(argc, argv);
 	if (app.sendMessage(message))
 		return 0;
-    
-    MainWindow mainWin;
+
+	MainWindow mainWin;
 	app.installEventFilter(&mainWin);
-    	
+
 	mainWin.handleMessage(message);
-    mainWin.show();
+	mainWin.show();
 
 	app.setActivationWindow(&mainWin, false);
-    QObject::connect(&app, SIGNAL(messageReceived(const QString&)), &mainWin, SLOT(handleMessage(const QString&)));
-	
-    return app.exec();
+	QObject::connect(&app, SIGNAL(messageReceived(const QString&)), &mainWin, SLOT(handleMessage(const QString&)));
+
+	return app.exec();
 }
