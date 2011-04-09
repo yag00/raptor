22fc3f7c8c768502e9701e59e01a57a6630dfc30
christophe.duvernois <christophe.duvernois@17a369b2-0695-9883-d3f2-36c5a1934f31>
2011-04-09 10:28:59 +0000

    add usage help and version console message

--- trunk/src/main.cpp
+++ trunk/src/main.cpp
@@ -1,16 +1,50 @@
+#include <iostream>
 #include <QApplication>
-#include <QtGui>
 #include <qtsingleapplication.h>
+#include <Qsci/qsciglobal.h>
 #include "mainwindow.h"
+
+namespace {
+	void version(){
+		std::cout << PACKAGE_NAME << " - " << PACKAGE_VERSION << std::endl;
+	}
+	void qsciVersion(){
+		version();
+		std::cout << "build on qscintilla " << QSCINTILLA_VERSION_STR << std::endl;
+	}
+	void help(){
+		std::cout << "Usage : raptor [args] [files ...] open specified file(s)" << std::endl;
+		std::cout << std::endl;
+		std::cout << "Arguments : " << std::endl;
+		std::cout << "\t" << "-v, --version" << "\t" << "show version information and quit" << std::endl;
+		std::cout << "\t" << "-h, --help" << "\t" << "show this message and quit" << std::endl;
+		std::cout << std::endl;
+	}
+}
 
 int main(int argc, char *argv[]) {
 	Q_INIT_RESOURCE(ressources);
 
 	QString message;
 	for (int a = 1; a < argc; ++a) {
-		message += argv[a];
-		if (a < argc-1)
-			message += ";";
+		QString arg = argv[a];
+		
+		if(arg[0] == '-'){
+			if(arg == "-v" || arg =="--version"){
+				version();
+				return 0;
+			}
+			if(arg == "--qsci-version"){
+				qsciVersion();
+				return 0;
+			}
+			help();
+			return 0;
+		}else{
+			message += arg;
+			if (a < argc-1)
+				message += ";";
+		}
 	}
 
 	//QApplication app(argc, argv);
