b657aabe792612e494dc7225988359add30a439f
christophe.duvernois <christophe.duvernois@17a369b2-0695-9883-d3f2-36c5a1934f31>
2011-04-09 17:01:59 +0000

    add console option

--- trunk/src/main.cpp
+++ trunk/src/main.cpp
@@ -2,9 +2,29 @@
 #include <QApplication>
 #include <qtsingleapplication.h>
 #include <Qsci/qsciglobal.h>
+#include "settings/settings.h"
 #include "mainwindow.h"
 
 namespace {
+	void displayOption(const std::string& shortOption, const std::string& fullOption, const std::string& desc){	
+		std::cout.flags(std::ios::right);
+		std::cout.width(2);
+		std::cout << "  ";
+		
+		std::cout.flags(std::ios::left);
+		std::cout.width(6);
+		if(shortOption.empty())
+			std::cout << "";
+		else
+			std::cout << ("-" + shortOption + ",");
+
+		std::cout << "--";
+		std::cout.flags(std::ios::left);
+		std::cout.width(20);
+		std::cout << fullOption;
+		std::cout << desc << std::endl;
+	}
+	
 	void version(){
 		std::cout << PACKAGE_NAME << " - " << PACKAGE_VERSION << std::endl;
 	}
@@ -13,18 +33,24 @@ namespace {
 		std::cout << "build on qscintilla " << QSCINTILLA_VERSION_STR << std::endl;
 	}
 	void help(){
-		std::cout << "Usage : raptor [args] [files ...] open specified file(s)" << std::endl;
+		std::cout << "Usage : " << std::endl;
+		std::cout << "  raptor [args] <files...> - Edit files" << std::endl;
 		std::cout << std::endl;
 		std::cout << "Arguments : " << std::endl;
-		std::cout << "\t" << "-v, --version" << "\t" << "show version information and quit" << std::endl;
-		std::cout << "\t" << "-h, --help" << "\t" << "show this message and quit" << std::endl;
+		displayOption("v", "version", "show version information and quit");
+		displayOption("h", "help", "show this message and quit");
+		displayOption("", "reset-lexers", "reset all lexers configuration to default");
 		std::cout << std::endl;
+	}
+	void resetLexer(){
+		Settings settings;
+		settings.remove("/Scintilla");
 	}
 }
 
 int main(int argc, char *argv[]) {
 	Q_INIT_RESOURCE(ressources);
-
+	
 	QString message;
 	for (int a = 1; a < argc; ++a) {
 		QString arg = argv[a];
@@ -38,6 +64,10 @@ int main(int argc, char *argv[]) {
 				qsciVersion();
 				return 0;
 			}
+			if(arg == "--reset-lexers"){
+				resetLexer();
+				return 0;
+			}			
 			help();
 			return 0;
 		}else{
