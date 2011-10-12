b301a6a23159db0d492bfd51fdf30109f9628134
christophe.duvernois <christophe.duvernois@17a369b2-0695-9883-d3f2-36c5a1934f31>
2011-10-12 22:17:37 +0000

    add feature session

---
+++ trunk/src/settings/SessionSettings.h
@@ -0,0 +1,45 @@
+/**
+ * This file is part of the raptor project.
+ *
+ * Copyright (C) 2011 Christophe Duvernois <christophe.duvernois@gmail.com>
+ *
+ * Raptor is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Raptor is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301 USA
+ */
+
+#ifndef __SESSION_SETTINGS_H__
+#define __SESSION_SETTINGS_H__
+
+#include <QSettings>
+
+class QAction;
+
+/** handle all session settings of the application */
+class SessionSettings : public QSettings{
+
+	Q_OBJECT
+
+	public:
+		SessionSettings(QObject* parent_ = 0);
+		~SessionSettings();
+
+		void save(const QString& session_, const QList<QStringList>& view_);
+		void load(const QString& session_, QList<QStringList>& view_);
+	
+		/** @return the number of documents in the session */
+		int getDocumentNumber(const QString& session_);
+
+};
+
+#endif // __SESSION_SETTINGS_H__
